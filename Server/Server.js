a3d3ce30d7e1f82eb5a720a3120dcfc1f1aa1856
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-06-01 19:38:55 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -34,4 +34,5 @@ app.use('/api/rooms', roomRoutes);
 
 const PORT = process.env.PORT || 3000 ;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+export default app;
